d0e5de8b344900f96707fa846c0104aed5baa019
chazzaoui <c.azzaoui@live.nl>
2020-07-16 20:31:02 +0200

    Initial Commit

---
+++ I am rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am rich
+//
+//  Created by Charaf  on 16/07/2020.
+//  Copyright © 2020 Charaf . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
